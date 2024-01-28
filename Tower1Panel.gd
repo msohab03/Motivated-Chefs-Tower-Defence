e33664d34dffa47618e215f4781a25059af88288
Satwik Gorla <st325640@dal.ca>
2024-01-28 01:40:50 -0400

    added tower placement UI and utility to drag and place towers

---
+++ Tower1Panel.gd
@@ -0,0 +1,23 @@
+extends Panel
+
+@onready var tower = preload("res://test_tower.tscn") #ENTER PATH TO TOWER SCENE HERE
+var currTile #keep for a bit?
+
+func _on_gui_input(event):
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#mouse click
+		add_child(tempTower)
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		get_child(1).global_position = event.global_position
+		#mouse moved
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		get_child(1).queue_free()
+		if get_child_count()>1:
+			get_child(1).queue_free()
+		#mouse click release
+		var source = get_tree().get_root().get_node("Level1/Towers")
+		source.add_child(tempTower)
+		tempTower.global_position = event.global_position
+		#tempTower.get_node("Area").hide()
